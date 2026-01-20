d869b2ac5ed632adbb66bbe3985371594c8f9986
Aishwarya M.B <aishwaryamb1997@gmail.com>
2026-01-20 16:17:03 +0530

    Initial commit: Angular RxJS demo project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ajax } from 'rxjs/ajax';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-rxjs-demo');
+
+  api$!: Observable<any>;
+
+  callApi() {
+    this.api$ = ajax({
+      url: 'https://httpbin.org/delay/1',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/text'
+      },
+      body: 'Hello'
+    });
+  }
 }
